721ca955eb416d0d5106dbee438da19fb3823d67
Pascal Iske <info@pascal-iske.de>
2018-06-19 16:13:10 +0200

    chore(tests): run module tests only

--- src/index.test.ts
+++ src/index.test.ts
@@ -36,7 +36,3 @@ export async function factory(): Promise<supertest.SuperTest<supertest.Test>> {
 test.beforeEach(async () => {
     // setup test context
 })
-
-test('init', async t => {
-    t.pass()
-})
